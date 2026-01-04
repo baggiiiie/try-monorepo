eb6618dc14a9ef9d9f9a6775c825117fc5f899f2
baggiiiie <yc@yingchaoooo.com>
2026-01-04 16:41:43 +0800

    [neetcode]remove from linked list

--- 2025-12-04-neetcode/remove-node-from-linked-list.py
+++ 2025-12-04-neetcode/remove-node-from-linked-list.py
@@ -5,7 +5,7 @@ def print_linked_list(head):
     while head:
         print(head.val, end=" -> ")
         head = head.next
-    print()
+    print("[null]")
 
 
 # Definition for singly-linked list.
@@ -20,18 +20,43 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        # brute force:
+        # - go through the list, check every time if the next n item is None
+        # - O(n^2)
+        # better?:
+        # - go through list to get length
+        # - find (len-n)-th node, point its previous.next to its next
+        # - O(n)?
+        if not head:
+            return None
+
+        dummy = ListNode(0, head)
+
+        right_ptr, left_ptr = head, dummy
+        for _ in range(n):
+            right_ptr = right_ptr.next
+
+        while right_ptr:
+            right_ptr = right_ptr.next
+            left_ptr = left_ptr.next
+        previous_node = left_ptr
+
+        assert previous_node is not None
+        assert previous_node.next is not None
+        previous_node.next = previous_node.next.next
+        return dummy.next
 
 
 if __name__ == "__main__":
     # Test cases
     s = Solution()
     test_cases = [
-        ListNode(2, ListNode(4, ListNode(6, ListNode(8)))),
-        ListNode(2, ListNode(4, ListNode(6, ListNode(8, ListNode(10, None))))),
-        ListNode(1, None),
+        (ListNode(2, ListNode(4, ListNode(6, ListNode(8)))), 1),
+        (ListNode(2, ListNode(4, ListNode(6, ListNode(8, ListNode(10, None))))), 3),
+        (ListNode(1, None), 1),
+        (ListNode(1, ListNode(2, None)), 2),
     ]
-    for test_case in test_cases:
-        s.removeNthFromEnd(test_case)
-        print_linked_list(test_case)
+    for linked_list, idx in test_cases:
+        head = s.removeNthFromEnd(linked_list, idx)
+        print_linked_list(head)
         print()
