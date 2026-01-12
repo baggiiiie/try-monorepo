ae1bf8358f274f0df9e5d64a5c627e82d871d06b
baggiiiie <yc@yingchaoooo.com>
2026-01-12 10:27:52 +0800

    [neetcode] sum two linked list

--- 2025-12-04-neetcode/sum-two-linked-list.py
+++ 2025-12-04-neetcode/sum-two-linked-list.py
@@ -22,51 +22,35 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        # addition needs to start from the end:
-        # 1. Reverse two lists
-        # 2. Add with carry, join result to new_list
-        # Question:
-        # - is the two list same length?
-        def reverse_list(head: Optional[ListNode]) -> Optional[ListNode]:
-            prev = None
-            curr = head
-            while curr:
-                next_node = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_node
-            return prev
-
-        list1, list2 = reverse_list(l1), reverse_list(l2)
-        curr = new_list = ListNode(0)
-
+        # loop through and sum
         carry = 0
-        while list1 and list2:
-            new_sum = list1.val + list2.val + carry
-            # max of new_sum = 9 + 9 + 1
-            carry = 1 if new_sum > 9 else 0
-            new_sum = new_sum % 10
-            curr.next = ListNode(new_sum)
-            list1, list2 = list1.next, list2.next
+        curr = new_head = ListNode(0)
+        while l1 or l2 or carry:
+            if not l1:
+                l1 = ListNode(0)
+            if not l2:
+                l2 = ListNode(0)
+            list_sum = l1.val + l2.val + carry  # maximum: 9+9+1 = 19
+            carry, list_sum = divmod(list_sum, 10)
+            curr.next = ListNode(list_sum)
+            l1, l2 = l1.next, l2.next
             curr = curr.next
-        # if one list still has next_node, add rest of the list up
-        rest = list1 or list2
-        if rest:
-            rest.val += carry
-        curr.next = rest
 
-        if carry:
-            curr.next = ListNode(carry)
-            return reverse_list(new_list)
-        return reverse_list(new_list.next)
+        return new_head.next
 
 
 if __name__ == "__main__":
     # Test cases
     s = Solution()
-    list1 = ListNode(1, ListNode(2, ListNode(9)))
-    list2 = ListNode(1, ListNode(3, ListNode(4)))
+    list1 = ListNode(1, ListNode(2, ListNode(3)))
+    list2 = ListNode(4, ListNode(5, ListNode(6)))
     print_linked_list(s.addTwoNumbers(list1, list2))
     list1 = ListNode(9)
     list2 = ListNode(9)
+    print_linked_list(s.addTwoNumbers(list1, list2))
+    list1 = ListNode(9, ListNode(9, ListNode(9)))
+    list2 = ListNode(9)
+    print_linked_list(s.addTwoNumbers(list1, list2))
+    list1 = ListNode(1, ListNode(8))
+    list2 = ListNode(0)
     print_linked_list(s.addTwoNumbers(list1, list2))
