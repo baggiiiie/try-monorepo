281c3e8397840261295a82cc52f3056823727092
baggiiiie <yc@yingchaoooo.com>
2026-02-17 19:33:34 +0800

    move cv-editor, configlock, and spliit-bot to independent project

--- cv-editor/server/go.mod
+++
@@ -1,5 +0,0 @@
-module cv-editor-server
-
-go 1.25.7
-
-require gopkg.in/yaml.v3 v3.0.1 // indirect
--- cv-editor/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import './index.css'
-import CvEditor from './pages/CvEditor'
-
-export default function App() {
-  return <CvEditor />
-}
